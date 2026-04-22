529afc294a7bab4ce83afbd070c74b78094eab33
Renan Willian <renanwilliandelima687@gmail.com>
2026-04-22 20:09:34 -0300

    atualizaçao

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ root.render(
        authorizationParams={{
          redirect_uri: Capacitor.isNativePlatform()
            ? 'com.zalio.app://dev-7tf743azyjk8acdg.us.auth0.com/capacitor/com.zalio.app/callback'
-           : window.location.origin
+           : window.location.origin + '/zalio'
        }}
      >
        <RouterProvider router={router} />
