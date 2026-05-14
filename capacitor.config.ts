6a919c715f4b6ee0b304d7f5c304542c75e35b16
Renan Willian <renanwilliandelima687@gmail.com>
2026-05-14 20:05:31 -0300

    Teste

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,13 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.rwl.zalio',
   appName: 'Zalio',
   webDir: 'build',
-  server: {
-    url: 'https://renanwlima.github.io/zalio',
-    allowNavigation: [
-      'dev-7tf743azyjk8acdg.us.auth0.com',
-      'renanwlima.github.io'
-    ]
-  }
 };
 
 export default config;
