2896833836a7501548dc47f808269974fff1ad5e
Renan Willian <renanwilliandelima687@gmail.com>
2026-09-17 20:39:19 -0300

    style: remove trailing whitespace from footer tag in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Capacitor } from '@capacitor/core';
 
 export default function Footer() {
   return (
-    <footer 
+    <footer
       className="footer no-print"
       style={{
         paddingBottom: Capacitor.isNativePlatform() ? 'calc(0.2rem + env(safe-area-inset-bottom, 0px))' : '0.2rem',
